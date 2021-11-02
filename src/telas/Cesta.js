06db39e72a8c8f133d2b89a7338a66540f1002dd
Carlos Gadelha <josecarlos.gadelha10@gmail.com>
2021-11-02 09:06:46 -0300

    add imagem topo

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import topo from '../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Cesta(){
-    return <Text>Cesta</Text>
+    return <>
+    <Image source={topo} style= {estilos.topo} />
+    <Text>Detalhes da Cesta</Text>
+    </>
 }
+
+const estilos = StyleSheet.create({
+    topo:{
+        width: "100%",
+        height: 578 / 768 * width,
+    }
+});
